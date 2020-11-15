3abf7399e79539b4aee4b8d3a28baae1f6154198
WingCH <jason021075@me.com>
2020-11-15 15:17:07 +0800

    Initial Commit

---
+++ Discover/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Discover
+//
+//  Created by CHAN Hong Wing on 15/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Discover/DiscoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscoverApp.swift
+//  Discover
+//
+//  Created by CHAN Hong Wing on 15/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct DiscoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
