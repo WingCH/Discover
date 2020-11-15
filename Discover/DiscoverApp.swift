5a54d88af592ec35cdf25a1bb682c245de6c01b3
WingCH <jason021075@me.com>
2020-11-15 18:03:16 +0800

    init TabbarView

--- Discover/DiscoverApp.swift
+++ Discover/DiscoverApp.swift
@@ -6,12 +6,45 @@
 //
 
 import SwiftUI
+import SwiftUIFlux
+
+let store = Store<AppState>(reducer: appStateReducer,
+                            middleware: [loggingMiddleware],
+                            state: AppState())
 
 @main
 struct DiscoverApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            StoreProvider(store: store) {
+                TabbarView()
+            }
+        }
+    }
+}
+
+struct TabbarView: View {
+    @State var selectedTab = Tab.discover
+
+    enum Tab: Int {
+        case discover
+    }
+
+    func tabbarItem(text: String, image: String) -> some View {
+        VStack {
+            Image(systemName: image)
+                .imageScale(.large)
+            Text(text)
+        }
+    }
+
+    var body: some View {
+        TabView(selection: $selectedTab) {
+
+            DiscoverView().tabItem{
+                self.tabbarItem(text: "Discover", image: "square.stack")
+            }.tag(Tab.discover)
+
         }
     }
 }
